eb09554fd3f00bccdc75b3126473c2a44dd861bc
zobaermd620 <zobaermd620@gmail.com>
2025-04-23 06:34:03 +0000

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,27 +1,20 @@
 # Copyright (C) 2017-2023 The Android Open Source Project
-# Copyright (C) 2014-2023 The Team Win LLC
 # SPDX-License-Identifier: Apache-2.0
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+# Installs GSI keys into ramdisk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Configure Virtual A/B
+# Virtual A/B & Compression
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# Configure virtual_ab compression.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
-# Configure launch_with_vendor_ramdisk.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Dynamic
+# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Virtual A/B
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
 
@@ -54,7 +47,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# API
+# API level
 PRODUCT_SHIPPING_API_LEVEL := 32
 
 # Boot control HAL
@@ -82,7 +75,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service.rc
 
-# Soong
+# Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
@@ -90,7 +83,7 @@ PRODUCT_PACKAGES += \
     create_pl_dev \
     create_pl_dev.recovery
 
-# Additional binaries & libraries needed for recovery
+# Recovery modules
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libkeymaster41 \
@@ -103,34 +96,27 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages.so
 
-
-
-# OrangeFox Recovery Configuration
+# PBRP Configuration
 PRODUCT_PACKAGES += \
-    orangefox \
+    pbrp \
     init.recovery.mt6768.rc \
     init.recovery.usb.rc \
     init.recovery.mt6768.rc.recovery
 
-# Copy necessary OrangeFox init scripts into recovery ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/fox/init.recovery.mt6768.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6768.rc \
     $(LOCAL_PATH)/recovery/fox/init.recovery.usb.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.usb.rc \
     $(LOCAL_PATH)/recovery/fox/init.recovery.mt6768.rc.recovery:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.mt6768.rc.recovery
 
-# OrangeFox Specific Settings
+# PBRP Specific Flags
+PBRP_SUPPORT_DEFAULT_THEME := true
+PBRP_USE_LOGD := true
+PBRP_DEVICE := true
+PBRP_INCLUDE_LPTOOLS := true
+PBRP_FORCE_PB_RECOVERY := true
+PBRP_MAINTAINER := your_nickname_here
 TW_DEFAULT_LANGUAGE := en
-OF_USE_GREEN_LED := 0
-OF_NO_TREBLE_COMPATIBILITY_CHECK := 1
-OF_PATCH_AVB20 := 1
-OF_CLASSIC_LEDS_FUNCTION := 1
-OF_DONT_PATCH_ENCRYPTED_DEVICE := 1
-OF_SKIP_FBE_DECRYPTION := 1
-OF_IGNORE_LOGICAL_MOUNT_ERRORS := true
-OF_KEEP_FORCED_ENCRYPTION := true
-OF_SUPPORT_ALL_BLOCK_OTA_UPDATES := true
-OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR := true
-OF_USE_MAGISKBOOT := 1
-OF_DISABLE_MIUI_OTA_BY_DEFAULT := true
-OF_ENABLE_LPTOOLS := true
-OF_ADVANCED_SECURITY := true
+TW_INCLUDE_FASTBOOTD := true
+
+# Optional: remove any OrangeFox-specific flags if not used
+# They are not harmful but no longer needed in PBRP
