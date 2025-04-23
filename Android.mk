acf6cdae83b8b3bc5b45cdfa31f0c31834f77b02
zobaermd620 <zobaermd620@gmail.com>
2025-04-23 06:44:12 +0000

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,12 +1,32 @@
 #
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
+# Copyright (C) 2025
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),pond)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifneq ($(filter pond lake,$(TARGET_DEVICE)),)
+
+# Include custom recovery init scripts
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.mt6768.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := recovery/pbrp/init.recovery.mt6768.rc
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.usb.rc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := recovery/pbrp/init.recovery.usb.rc
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.recovery.mt6768.rc.recovery
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := recovery/pbrp/init.recovery.mt6768.rc.recovery
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
 endif
